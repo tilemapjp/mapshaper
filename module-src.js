d0266a24063e0d0e5747e2ba88562b0afd7d50dd
KOHEI OTSUKA <kochizufan@gmail.com>
2018-07-25 14:41:22 +0900

    Original Build

---
+++ module-src.js
@@ -0,0 +1,7 @@
+/* @require mapshaper-segment-intersection */
+var VERSION;
+module.exports = {
+  ArcCollection: ArcCollection,
+  dedupIntersections: internal.dedupIntersections,
+  findSegmentIntersections: internal.findSegmentIntersections
+};
