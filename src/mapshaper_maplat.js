28466f467daea373620459366c3f5da4f58093e3
OHTSUKA Ko-hei <kochizufan@gmail.com>
2018-11-04 14:10:07 +0900

    Maplat版ビルドスクリプト

---
+++ src/mapshaper_maplat.js
@@ -0,0 +1,21 @@
+/* @requires
+mapshaper-segment-intersection
+*/
+
+api.internal = internal;
+this.mapshaper = api;
+
+// Expose internal objects for testing
+utils.extend(api.internal, {
+  ArcCollection: ArcCollection,
+  ArcIter: ArcIter
+});
+
+if (typeof define === "function" && define.amd) {
+    //define("mapshaper", api);
+    define([], function() {
+        return api;
+    });
+} else if (typeof module === "object" && module.exports) {
+    module.exports = api;
+}
